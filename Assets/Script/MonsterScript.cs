1a9f506cc8d00f442f889bb9a992e25c3b4d7e2a
煜 周 <zyu930@qq.com>
2018-03-15 17:58:16 +0800

    NPC对话完成
    完成了跟NPC对话的功能

---
+++ Assets/Script/MonsterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour {
+
+	public int countDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D visitor)
+	{
+		if(visitor.gameObject.tag == "PlayerField"){
+			Debug.Log("Warning");
+			Attack();
+		}
+	}
+
+	IEnumerator Prepare(int times){
+		for(int i = times;i>0;i--){
+			Debug.Log(i);
+			yield return new WaitForSeconds(1);
+		}
+	}
+
+	void Attack(){
+		StartCoroutine(Prepare(countDown));
+		Debug.Log("Attack");
+	}
+}
---
+++ Assets/Script/NPCListener.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCListener : MonoBehaviour {
+
+	public string words;
+	bool talk = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.J) & talk){
+			Debug.Log(words);
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D Visitor)
+	{
+		if(Visitor.gameObject.tag=="PlayerField"){
+			if(talk!=true){
+				talk = true;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D Visitor)
+	{
+		if(Visitor.gameObject.tag=="PlayerField"){
+			talk = false;
+		}
+	}
+}
