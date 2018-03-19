dd9be6448637bc0a2917b4000df99ac3970a09d3
miflyc <jin_bin_@126.com>
2018-03-19 09:38:14 +0800

    demo123
    demotest

---
+++ Assets/Script/Gamemode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemode : MonoBehaviour {
+
+	public bool gameMode = false;
+	public GameObject inside;
+
+	// Use this for initialization
+	void Start () {
+		gameMode = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameMode){
+			if(inside.activeInHierarchy == false){
+				inside.SetActive(true);
+			}
+		}else{
+			if(inside.activeInHierarchy == true){
+				inside.SetActive(false);
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.C)){
+			ChangeGameMode();
+		}
+	}
+
+	public void ChangeGameMode(){
+		if(gameMode){
+			gameMode = false;
+		}else{
+			gameMode = true;
+		}
+	}
+}
