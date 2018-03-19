da041fcbf16c8c440ceb5fafc8a1ff6e413b154d
煜 周 <zyu930@qq.com>
2018-03-19 15:49:20 +0800

    修bug
    修复了一个玩家可以同时触发两个怪物倒数的bug

--- Assets/Script/MonsterScript.cs
+++ Assets/Script/MonsterScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MonsterScript : MonoBehaviour {
 
 	public int countDown;
-
+	bool attack = true;
 
 	// Use this for initialization
 	void Start () {
@@ -20,19 +20,23 @@ public class MonsterScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D Visitor)
 	{
 		if(Visitor.gameObject.tag == "PlayerField"){
-			Debug.Log("Warning");
-			StartCoroutine(Attack(countDown));
+			if(attack){
+				Debug.Log("Warning");
+				StartCoroutine(Attack(countDown));
+			}
 		}
 	}
 
 	
 	IEnumerator Attack(int times){
 		if(times>0){
+			attack = false;
 			Debug.Log(times);
 			yield return new WaitForSeconds(1);
 			StartCoroutine(Attack(times-1));
 		}else{
 			Debug.Log("Attack");
+			attack = true;
 		}
 	}
 
