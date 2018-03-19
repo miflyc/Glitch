aff4330e446bcea8bee89b9289ca44b39bf0fec5
煜 周 <zyu930@qq.com>
2018-03-19 09:53:54 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/miflyc/Glitch""
    This reverts commit fc5886b70bbe6bfc30d800f90e4e43aba0314b76.

---
+++ Assets/Script/MonsterScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour {
+
+	public int countDown;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D Visitor)
+	{
+		if(Visitor.gameObject.tag == "PlayerField"){
+			Debug.Log("Warning");
+			Attack();
+		}
+	}
+
+	
+	IEnumerator Prepare(int times){
+		Debug.Log(times);
+		yield return new WaitForSeconds(1);
+	}
+
+	void Attack(){
+		for(int i = countDown;i>0;i--){
+			StartCoroutine(Prepare(i));
+		}
+		Debug.Log("Attack");
+	}
+}
