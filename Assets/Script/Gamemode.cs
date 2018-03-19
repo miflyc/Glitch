fc5886b70bbe6bfc30d800f90e4e43aba0314b76
煜 周 <zyu930@qq.com>
2018-03-19 09:42:59 +0800

    Revert "Merge branch 'master' of https://github.com/miflyc/Glitch"
    This reverts commit 611ea0c31e9191025c1f4a42e85d283c3e91af2a, reversing
    changes made to dd9be6448637bc0a2917b4000df99ac3970a09d3.

--- Assets/Script/Gamemode.cs
+++ Assets/Script/Gamemode.cs
@@ -5,25 +5,15 @@ using UnityEngine;
 public class Gamemode : MonoBehaviour {
 
 	public bool gameMode = false;
-<<<<<<< HEAD
 	public GameObject inside;
-=======
-	MobileGlitchCameraShader glitchShader;
->>>>>>> 17c8a21fa2bb4395d81f22baa463c595de777b53
 
 	// Use this for initialization
 	void Start () {
 		gameMode = false;
-<<<<<<< HEAD
-=======
-		glitchShader = GetComponent<MobileGlitchCameraShader>();
-		glitchShader.enabled = false;
->>>>>>> 17c8a21fa2bb4395d81f22baa463c595de777b53
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
 		if(gameMode){
 			if(inside.activeInHierarchy == false){
 				inside.SetActive(true);
@@ -33,35 +23,16 @@ public class Gamemode : MonoBehaviour {
 				inside.SetActive(false);
 			}
 		}
-=======
->>>>>>> 17c8a21fa2bb4395d81f22baa463c595de777b53
 		if(Input.GetKeyDown(KeyCode.C)){
 			ChangeGameMode();
 		}
 	}
 
 	public void ChangeGameMode(){
-<<<<<<< HEAD
 		if(gameMode){
 			gameMode = false;
 		}else{
 			gameMode = true;
 		}
 	}
-=======
-		gameMode=!gameMode;
-		StartCoroutine(GlitchEffect());
-	}
-
-	public void Glitch(){
-		glitchShader.enabled = !glitchShader.enabled;
-	}
-
-	IEnumerator GlitchEffect()
-    {
-		Glitch();
-        yield return new WaitForSeconds(1);
-		Glitch();
-    }
->>>>>>> 17c8a21fa2bb4395d81f22baa463c595de777b53
 }
