d2a0f0badd83c4c211781664dffc479b1aa59848
煜 周 <zyu930@qq.com>
2018-03-19 09:32:34 +0800

    按左键瞬移写好了

---
+++ Assets/Script/Crasher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D enemy)
+	{
+		if(enemy.gameObject.tag == "enemy"){
+			Debug.Log("Beat you!");
+		}
+	}
+	
+}
