8a580783812de2c611f56a216f378c826faccaa1
煜 周 <zyu930@qq.com>
2018-04-04 17:02:06 +0800

    增加了小怪物的一些基本功能
    但是还是要改一下冲刺的函数，现在的效果很垃圾

--- Assets/Script/EnemyScript/MonsterScript.cs
+++ Assets/Script/EnemyScript/MonsterScript.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 public class MonsterScript : MonoBehaviour {
 
 	public int countDown;
-	bool attack = true;
 	public int hp = 10;
+	public float atkRange = 2.0f;
+	public float dashSpeed = 0.3f,deltaSpeed = 0.05f;
+	public GameObject player;
+	float timeCount = 0,nowSpeed;
+	private enum EnemyStates{
+		Idle,
+		Attacking,
+		Dead,
+	}
+	private EnemyStates currState;
+	bool targetOnYourRight;
 
 	// Use this for initialization
 	void Start () {
-		
+		currState = EnemyStates.Idle;
+		nowSpeed = dashSpeed;
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(hp<=0){
+			currState = EnemyStates.Dead;
 			Die();
+		}else if(currState == EnemyStates.Attacking){
+			timeCount+=Time.deltaTime;
+			nowSpeed-=Time.deltaTime*deltaSpeed;
+			Dash(targetOnYourRight);
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D Visitor)
 	{
 		if(Visitor.gameObject.tag == "PlayerField"){
-			if(attack){
-				Debug.Log("Warning");
+			if(currState!=EnemyStates.Attacking){
+				Debug.Log("Warning");		//播放准备动画
+				timeCount = 0.0f;
+				nowSpeed = dashSpeed;
+				Vector3 targetPoint = player.transform.position;
+				targetOnYourRight = false;
+				if(targetPoint.x>transform.position.x)
+					targetOnYourRight = true;
 				StartCoroutine(Attack(countDown));
 			}
 		}
@@ -33,17 +56,26 @@ public class MonsterScript : MonoBehaviour {
 	
 	IEnumerator Attack(int times){
 		if(times>0){
-			attack = false;
+			currState = EnemyStates.Attacking;
 			Debug.Log(times);
 			yield return new WaitForSeconds(1);
 			StartCoroutine(Attack(times-1));
 		}else{
-			Debug.Log("Attack");
-			attack = true;
+			Debug.Log("Attack");			//修改为怪物的攻击方式
+			currState = EnemyStates.Idle;
 		}
 	}
 	
 	void Die(){
 		Destroy(gameObject);
+	}
+
+	void Dash(bool targetOnYourRight){
+		Debug.Log("Dash");
+		if(targetOnYourRight){
+			transform.Translate(Vector3.Lerp(new Vector3(0,0,0),new Vector3(atkRange,0,0),Mathf.Clamp(timeCount*nowSpeed,0.0f,1.0f)));
+		}else{
+			transform.Translate(Vector3.Lerp(new Vector3(0,0,0),new Vector3(-atkRange,0,0),Mathf.Clamp(timeCount*nowSpeed,0.0f,1.0f)));
+		}
 	}
 }
